b1db0654d0896a29a786ceec3060270f1e209ff9
chanif <chanifuddin@live.com>
2016-06-10 10:33:25 +0700

    first commit

---
+++ backview/C4002/BUTTON_HOLE_THREAD/cuff_casualSingleButton/rename.php
@@ -0,0 +1,20 @@
+<?php
+function rename_win($oldfile,$newfile) {
+    if (!rename($oldfile,$newfile)) {
+        if (copy ($oldfile,$newfile)) {
+            unlink($oldfile);
+            return TRUE;
+        }
+        return FALSE;
+    }
+    return TRUE;
+}
+
+if ($handle = opendir('.')) {
+    while (false !== ($fileName = readdir($handle))) {
+        $newName = str_replace("lubangKancing_casualSingleButton_","",$fileName);
+        rename_win($fileName, $newName);
+    }
+    closedir($handle);
+}
+?>
